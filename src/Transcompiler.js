5adbd284a9fd20c621941de29b04a10841d90d39
Joshua Weinstein <jweinstein@splunk.com>
2016-06-17 11:48:10 -0700

    splitter

--- src/Transcompiler.js
+++ src/Transcompiler.js
@@ -1,1 +1,15 @@
 //Main Transcompiler file
+
+
+function tokenize(code){
+	var tokens = [];
+	var splits = code.split(/(".*?")|(,)|([a-zA-Z]+)|([0-9]+)|(\()|(\))|(\[)|(\])|(\{)|(\})| /);
+	for(var i=0;i<splits.length;i++){
+		if(splits[i]){
+			tokens.push(splits[i]);
+		}
+	}
+	return tokens;
+}
+
+console.log(tokenize("vote(foo(bar(4, 5)))"))
